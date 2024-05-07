00eae6a5adc33d80a25969c8be75d55a6b1dc687
swan-htet1 <swamhtetnaing27@gmail.com>
2024-05-07 13:05:08 +0630

    pushing to render

--- OnlineVotingSystem/settings.py
+++ OnlineVotingSystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^tm3@f!kx)$ytm^urm7%jfv&!tbg$*n8i81dz5wte7i(uo*%@)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
