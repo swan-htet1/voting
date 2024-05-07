9dfada16b7cfc4dacd6295781017aafdf5f99b80
swan-htet1 <swamhtetnaing27@gmail.com>
2024-05-07 13:47:13 +0630

    push render

--- OnlineVotingSystem/settings.py
+++ OnlineVotingSystem/settings.py
@@ -122,6 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
