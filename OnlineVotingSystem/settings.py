33dc606bc32452f2529a99996a7a0614e721eb3c
swan-htet1 <swamhtetnaing27@gmail.com>
2024-05-18 16:34:14 +0630

    Your comment

--- OnlineVotingSystem/settings.py
+++ OnlineVotingSystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^tm3@f!kx)$ytm^urm7%jfv&!tbg$*n8i81dz5wte7i(uo*%@)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.1','localhost','voting-zra2.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+ 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
